560b259663dcb5ad2e508b7f158987c0ca16ef64
Tuan Dat <datto25082003@gmail.com>
2025-04-13 00:40:05 +0700

    create project backend and test connection mysql

---
+++ oceanfresh_backend/src/main/java/com/tuandat/oceanfresh_backend/OceanfreshBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tuandat.oceanfresh_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanfreshBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OceanfreshBackendApplication.class, args);
+	}
+
+}
---
+++ oceanfresh_backend/src/test/java/com/tuandat/oceanfresh_backend/OceanfreshBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuandat.oceanfresh_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceanfreshBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
