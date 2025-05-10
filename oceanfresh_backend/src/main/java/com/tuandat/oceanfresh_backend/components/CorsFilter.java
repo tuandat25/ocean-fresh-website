026e6a9699db88258469cadc750ebfef6efcb656
Tuan Dat <datto25082003@gmail.com>
2025-05-11 05:36:00 +0700

    Cors Filter

---
+++ oceanfresh_backend/src/main/java/com/tuandat/oceanfresh_backend/components/CorsFilter.java
@@ -0,0 +1,33 @@
+package com.tuandat.oceanfresh_backend.components;
+
+
+import java.io.IOException;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+//định nghĩa một bean và đồng thời chỉ định một tên cụ thể cho bean đó
+@Component("customCorsFilter")
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
+        response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
+        if ("OPTIONS".equals(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}
